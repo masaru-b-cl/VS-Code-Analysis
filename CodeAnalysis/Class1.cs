d9819b525fc2826c07f72753265dab914ba68858
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2014-12-28 08:01:54 +0900

    initial commit

---
+++ CodeAnalysis/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codeAnalysis
+{
+  public class someClass
+  {
+    public void doSomething(int count, int i)
+    {
+      var newCount = countUp(count);
+    }
+
+    private int countUp(int count)
+    {
+      return count + 1;
+    }
+
+    private static void UnUseMethod()
+    {
+    }
+  }
+}
